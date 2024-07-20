f14ff1307016d2940b608556791bd22d8a4b1cfb
Tyler <tylercarrol@hotmail.com>
2024-07-20 17:06:38 -0400

    Create method to check if versions are equivalent (#3)
    I.e. if they are the exact same, or if a component is undefined, consider 0 equivalent

---
+++ TJC.VersionExtensions.Tests/EquivalentVersionExtensionsTests.cs
@@ -0,0 +1,148 @@
+﻿namespace TJC.VersionExtensions.Tests;
+
+
+[TestClass]
+public class EquivalentVersionExtensionsTests
+{
+    #region Equivalent (True) 
+
+    [TestMethod]
+    public void Equivalent_AllDefinedSame_ReturnsTrue()
+    {
+        var version1 = new Version(1, 2, 3, 4);
+        var version2 = new Version(1, 2, 3, 4);
+
+        var result = version1.Equivalent(version2);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void Equivalent_MajorMinorBuildDefined_ReturnsTrue()
+    {
+        var version1 = new Version(1, 2, 3);
+        var version2 = new Version(1, 2, 3, 0);
+
+        var result = version1.Equivalent(version2);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void Equivalent_MajorMinorDefined_ReturnsTrue()
+    {
+        var version1 = new Version(1, 2);
+        var version2 = new Version(1, 2, 0, 0);
+
+        var result = version1.Equivalent(version2);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void Equivalent_NoneDefined_ReturnsTrue()
+    {
+        var version1 = new Version();
+        var version2 = new Version(0, 0, 0, 0);
+
+        var result = version1.Equivalent(version2);
+
+        Assert.IsTrue(result);
+    }
+
+    #endregion
+
+    #region Equivalent (False)
+
+    [TestMethod]
+    public void Equivalent_DifferentMajors_ReturnsFalse()
+    {
+        var version1 = new Version(1, 0, 0, 0);
+        var version2 = new Version(2, 0, 0, 0);
+
+        var result = version1.Equivalent(version2);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void Equivalent_DifferentMinors_ReturnsFalse()
+    {
+        var version1 = new Version(1, 2, 0, 0);
+        var version2 = new Version(1, 3, 0, 0);
+
+        var result = version1.Equivalent(version2);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void Equivalent_DifferentBuilds_ReturnsFalse()
+    {
+        var version1 = new Version(1, 2, 3, 0);
+        var version2 = new Version(1, 2, 4, 0);
+
+        var result = version1.Equivalent(version2);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void Equivalent_DifferentRevisions_ReturnsFalse()
+    {
+        var version1 = new Version(1, 2, 3, 4);
+        var version2 = new Version(1, 2, 3, 5);
+
+        var result = version1.Equivalent(version2);
+
+        Assert.IsFalse(result);
+    }
+
+    #endregion
+
+    #region Is Zero
+
+
+
+    [TestMethod]
+    public void IsZero_NoneDefined_ReturnsTrue()
+    {
+        var version = new Version();
+
+        var result = version.IsZero();
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IsZero_MajorMinorDefined_ReturnsTrue()
+    {
+        var version = new Version(0, 0);
+
+        var result = version.IsZero();
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IsZero_AllDefined_ReturnsTrue()
+    {
+        var version = new Version(0, 0, 0, 0);
+
+        var result = version.IsZero();
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IsZero_NonZeroVersion_ReturnsFalse()
+    {
+        var version = new Version(0, 1, 0, 0);
+
+        var result = version.IsZero();
+
+        Assert.IsFalse(result);
+    }
+
+    #endregion
+}
---
+++ TJC.VersionExtensions/EquivalentVersionExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace TJC.VersionExtensions;
+
+public static class EquivalentVersionExtensions
+{
+    /// <summary>
+    /// Check if Version Number is Equivalent to Zero.
+    /// </summary>
+    /// <param name="version"></param>
+    /// <returns></returns>
+    public static bool IsZero(this Version version) =>
+        version.Equivalent(new Version(0, 0, 0, 0));
+
+    /// <summary>
+    /// Check if two versions are equivalent.
+    /// </summary>
+    /// <param name="version1"></param>
+    /// <param name="version2"></param>
+    /// <returns></returns>
+    public static bool Equivalent(this Version version1, Version version2) =>
+        VersionComponentEquivalent(version1.Major, version2.Major)
+     && VersionComponentEquivalent(version1.Minor, version2.Minor)
+     && VersionComponentEquivalent(version1.Build, version2.Build)
+     && VersionComponentEquivalent(version1.Revision, version2.Revision);
+
+    /// <summary>
+    /// Allows for differing values so long as they are both either 0 or -1 (since -1 means the value wasn't defined)
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    private static bool VersionComponentEquivalent(int num1, int num2) =>
+        num1 == num2
+        || (num1 == -1 && num2 == 0)
+        || (num1 == 0 && num2 == -1);
+}
