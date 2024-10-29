56ae45108d7333a6e1f1d52c3f179873138db54d
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-28 21:27:45 -0400

    ✨ feature: Add MegaLinter Workflow (#55)
    * ✨ feature: Add MegaLinter Workflow
    
    * 🚨 tests: Reduce Duplication in Tests
    
    * 🧹 chore: [MegaLinter] Apply [1] automatic fixes (#56)

--- TJC.VersionExtensions.Tests/IncrementVersionExtensionsTests.cs
+++ TJC.VersionExtensions.Tests/IncrementVersionExtensionsTests.cs
@@ -9,79 +9,63 @@ public class IncrementVersionExtensionsTests
     public void IncrementMajor_IncrementsMajorAndSetsTheFollowingToZero()
     {
         var newVersion = _version.IncrementMajor();
-        Assert.AreEqual(2, newVersion.Major);
-        Assert.AreEqual(0, newVersion.Minor);
-        Assert.AreEqual(0, newVersion.Build);
-        Assert.AreEqual(0, newVersion.Revision);
+        AssertVersion(newVersion, 2, 0, 0, 0);
     }
 
     [TestMethod]
     public void IncrementMinor_IncrementsMinorAndSetsTheFollowingToZero()
     {
         var newVersion = _version.IncrementMinor();
-        Assert.AreEqual(1, newVersion.Major);
-        Assert.AreEqual(3, newVersion.Minor);
-        Assert.AreEqual(0, newVersion.Build);
-        Assert.AreEqual(0, newVersion.Revision);
+        AssertVersion(newVersion, 1, 3, 0, 0);
     }
 
     [TestMethod]
     public void IncrementBuild_IncrementsBuildAndSetsRevisionToZero()
     {
         var newVersion = _version.IncrementBuild();
-        Assert.AreEqual(1, newVersion.Major);
-        Assert.AreEqual(2, newVersion.Minor);
-        Assert.AreEqual(4, newVersion.Build);
-        Assert.AreEqual(0, newVersion.Revision);
+        AssertVersion(newVersion, 1, 2, 4, 0);
     }
 
     [TestMethod]
     public void IncrementRevision_IncrementsRevisionOnly()
     {
         var newVersion = _version.IncrementRevision();
-        Assert.AreEqual(1, newVersion.Major);
-        Assert.AreEqual(2, newVersion.Minor);
-        Assert.AreEqual(3, newVersion.Build);
-        Assert.AreEqual(5, newVersion.Revision);
+        AssertVersion(newVersion, 1, 2, 3, 5);
     }
 
     [TestMethod]
     public void IncrementMajorType_IncrementsMajorAndSetsTheFollowingToZero()
     {
         var newVersion = _version.Increment(VersionComponents.Major);
-        Assert.AreEqual(2, newVersion.Major);
-        Assert.AreEqual(0, newVersion.Minor);
-        Assert.AreEqual(0, newVersion.Build);
-        Assert.AreEqual(0, newVersion.Revision);
+        AssertVersion(newVersion, 2, 0, 0, 0);
     }
 
     [TestMethod]
     public void IncrementMinorType_IncrementsMinorAndSetsTheFollowingToZero()
     {
         var newVersion = _version.Increment(VersionComponents.Minor);
-        Assert.AreEqual(1, newVersion.Major);
-        Assert.AreEqual(3, newVersion.Minor);
-        Assert.AreEqual(0, newVersion.Build);
-        Assert.AreEqual(0, newVersion.Revision);
+        AssertVersion(newVersion, 1, 3, 0, 0);
     }
 
     [TestMethod]
     public void IncrementBuildType_IncrementsBuildAndSetsRevisionToZero()
     {
         var newVersion = _version.Increment(VersionComponents.Build);
-        Assert.AreEqual(1, newVersion.Major);
-        Assert.AreEqual(2, newVersion.Minor);
-        Assert.AreEqual(4, newVersion.Build);
-        Assert.AreEqual(0, newVersion.Revision);
+        AssertVersion(newVersion, 1, 2, 4, 0);
     }
 
     [TestMethod]
     public void IncrementRevisionType_IncrementsRevisionOnly()
     {
         var newVersion = _version.Increment(VersionComponents.Revision);
-        Assert.AreEqual(1, newVersion.Major);
-        Assert.AreEqual(2, newVersion.Minor);
-        Assert.AreEqual(3, newVersion.Build);
-        Assert.AreEqual(5, newVersion.Revision);
+        AssertVersion(newVersion, 1, 2, 3, 5);
+    }
+
+    private void AssertVersion(Version version, int major, int minor, int build, int revision)
+    {
+        Assert.AreEqual(major, version.Major);
+        Assert.AreEqual(minor, version.Minor);
+        Assert.AreEqual(build, version.Build);
+        Assert.AreEqual(revision, version.Revision);
     }
 }
